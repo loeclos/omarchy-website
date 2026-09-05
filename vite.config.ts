3b98e9f0e11dd379f3ac5dd9932c8ff822d6027a
gleb <116607327+loeclos@users.noreply.github.com>
2026-09-05 15:57:57 -0700

    chore: prepare for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -10,7 +11,8 @@ export default defineConfig({
   plugins: [
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
+    nitro(),
     viteReact(),
     tailwindcss(),
   ],
-})
+});
